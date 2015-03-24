3dd8ecf80776615a01c93fcc6591ca537324200f
flyingmaskin <maaugr@gmail.com>
2015-03-24 22:33:29 +0100

    hjelpemelding and shit

--- Client.py
+++ Client.py
@@ -26,7 +26,8 @@ class Client:
         
         self.msgRec.start()
         self.connection.connect((self.host, self.server_port))
-        
+        print "Velkommen til denne chatte-appen. Skriv #logginn hvis du vil logge inn. Hvis du har problemer skriv #hjelp"
+        print ""
         while True:
             command = raw_input('')
             if command == "#logginn":
@@ -41,34 +42,46 @@ class Client:
                     print("Ikke bruk norske bokstaver.")
                     continue
             elif command == "#hjelp":
-                print "hjelpmelding"
-            elif command == "#navn":
-                #faa navn fra server
-                data = {"request":"names","content":""}
-                package = json.dumps(data)
-                self.send_payload(package)
-            elif command == "#loggut" and self.hasLoggedOn:
-                data = {"request":"logout","content":username}
-                package = json.dumps(data)
-                self.send_payload(package)
-                self.disconnect()
-                self.hasLoggedOn = False
-            elif command == "#historie" and self.hasLoggedOn:
-                #faa historie fra serveren
-                data = {"request":"history","content":""}
-                package = json.dumps(data)
-                self.send_payload(package)
+                print ""
+                print "Jeg ser at du trenger hjelp, her er alle kommandoene du kan bruke"
+                print ""
+                print "#logginn : denne bruker du hvis du vil logge inn på serveren"
+                print "#navn : vis alle brukere i chatten"
+                print "#historie : en liste over alle meldingene på serveren"
+                print "#logout : denne bruker du hvis du vil logge ut av serveren"
+                print "#hjelp : hvis du trenger hjelp bruk denne"
+                print ""
             else:
-                if self.hasLoggedOn:
-                    data = {"request":"msg","content":command}
-                    try:
-                        package = json.dumps(data)
-                        self.send_payload(package)
-                    except UnicodeDecodeError:
-                        print("Ikke bruk norske bokstaver.")
-                        continue
+                if not self.hasLoggedOn:
+                    print "Du har ikke logget inn enda. Skriv #logginn"
+                    pass
+                elif command == "#navn":
+                    #faa navn fra server
+                    data = {"request":"names","content":""}
+                    package = json.dumps(data)
+                    self.send_payload(package)
+                elif command == "#loggut" and self.hasLoggedOn:
+                    data = {"request":"logout","content":username}
+                    package = json.dumps(data)
+                    self.send_payload(package)
+                    self.disconnect()
+                    self.hasLoggedOn = False
+                elif command == "#historie" and self.hasLoggedOn:
+                    #faa historie fra serveren
+                    data = {"request":"history","content":""}
+                    package = json.dumps(data)
+                    self.send_payload(package)
                 else:
-                    print "Du maa vaere logget inn for aa gjore det"
+                    if self.hasLoggedOn:
+                        data = {"request":"msg","content":command}
+                        try:
+                            package = json.dumps(data)
+                            self.send_payload(package)
+                        except UnicodeDecodeError:
+                            print("Ikke bruk norske bokstaver.")
+                            continue
+                    else:
+                        print "Du maa vaere logget inn for aa gjore det"
                 
 
     def disconnect(self):
